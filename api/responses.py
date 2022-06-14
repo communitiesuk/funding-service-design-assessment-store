21c1a4b2b66a8769f3dd4121591f231007013cc9
Ram <rmn24in@gmail.com>
2022-06-14 14:02:15 +0100

    ⚡️  key status added to response

--- api/responses.py
+++ api/responses.py
@@ -14,6 +14,7 @@ def assessment_response(assessment: Assessment, code: int = 200):
         make_response(
             {
                 "id": assessment.id,
+                "status": assessment.status,
                 "applicationId": assessment.application_id,
             }
         ),
