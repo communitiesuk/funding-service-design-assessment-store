94eb977a47fe278446d0dcd0b63bcd5f0973282f
Indi Harrington <indigoharrington@gmail.com>
2022-11-21 22:35:26 +0000

    ✏️ missing dot

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,7 +1,7 @@
-from test_db_function import test_jsonb_blob_immutable
-from test_db_function import test_non_blob_columns_mutable
-from test_db_function import test_select_field_by_id
-from test_routes import test_gets_all_apps_for_fund_round
+from .test_db_function import test_jsonb_blob_immutable
+from .test_db_function import test_non_blob_columns_mutable
+from .test_db_function import test_select_field_by_id
+from .test_routes import test_gets_all_apps_for_fund_round
 
 __all__ = [
     test_gets_all_apps_for_fund_round,
