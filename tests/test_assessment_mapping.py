aaaec4338fd544e910bceb8ce7e8b0ea130f1c5d
Adam <adam.wallace@communities.gov.uk>
2022-11-25 16:43:29 +0000

    rename test

--- tests/test_assessment_mapping.py
+++ tests/test_assessment_mapping.py
@@ -3,5 +3,5 @@ from config.mappings.assessment_mapping_schema import top_level_assessment_mappi
 from config import Config
 
 
-def test_mapping():
+def test_assessment_mapping_conforms_to_schema():
     assert validate(instance=Config.COF_R2W2_ASSESSMENT_MAPPING, schema=schema) == None
