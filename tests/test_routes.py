c03c477f473a3832480d9c01e1f10dd39243a91e
Indi Harrington <indigoharrington@gmail.com>
2022-12-01 15:21:09 +0000

    🚨 isort

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -1,6 +1,5 @@
 from db.models.assessment_record.assessment_records import AssessmentRecord
-from tests._helpers import get_random_row
-from tests._helpers import get_rows_by_filters
+from tests._helpers import get_random_row, get_rows_by_filters
 
 
 def test_gets_all_apps_for_fund_round(request, client):
