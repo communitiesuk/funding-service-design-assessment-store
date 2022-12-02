cb44a32a6a87262fbc9f5110742a4841ca5359da
Adam <adam.wallace@communities.gov.uk>
2022-12-02 11:58:42 +0000

    added route (with tests) to handle sub criteria request

--- tests/test_assessment_mapping.py
+++ tests/test_assessment_mapping.py
@@ -1,7 +1,12 @@
 from jsonschema import validate
-from config.mappings.assessment_mapping_schema import top_level_assessment_mapping_schema as schema
+from config.mappings.assessment_mapping_schema import (
+    top_level_assessment_mapping_schema as schema,
+)
 from config import Config
 
 
 def test_assessment_mapping_conforms_to_schema():
-    assert validate(instance=Config.COF_R2W2_ASSESSMENT_MAPPING, schema=schema) == None
+    assert (
+        validate(instance=Config.COF_R2W2_ASSESSMENT_MAPPING, schema=schema)
+        == None
+    )
