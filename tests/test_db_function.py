f9745518b7f6dbde9ce8245ef4a83871b946659d
Gio Karunakaran <gio.karunakaran@version1.com>
2022-12-02 14:41:43 +0000

    testing for scores db queries

--- tests/test_db_function.py
+++ tests/test_db_function.py
@@ -4,6 +4,11 @@ import sqlalchemy
 from db.models.assessment_record.assessment_records import AssessmentRecord
 from db.queries.assessment_records import find_answer_by_key_runner
 from tests._helpers import get_random_row
+import time
+from db import db
+
+from db.queries.scores.queries import create_just_score_for_application_sub_crit
+from db.queries.scores.queries import get_latest_just_score_for_application_sub_crit
 
 
 def test_select_field_by_id():
@@ -53,3 +58,43 @@ def test_non_blob_columns_mutable(db_session):
         db_session.commit()
     except sqlalchemy.exc.InternalError:
         raise AssertionError
+
+
+def test_create_scores_for_application_su_crit():
+    picked_row = get_random_row(AssessmentRecord)
+    application_id = picked_row.application_id
+    sub_criteria_id = "app-info"
+    
+    assessment_payload = {
+        "application_id": application_id,
+        "sub_criteria_id": sub_criteria_id,
+        "score": 3,
+        "justification": "bang average",
+        "user_id": "test"
+    }
+    score_metadata = create_just_score_for_application_sub_crit(**assessment_payload)
+
+    assert len(score_metadata) == 7
+    assert score_metadata.get("date_created")
+    assert score_metadata.get("score") == 3
+
+
+def test_scores_for_application_sub_crit():
+    picked_row = get_random_row(AssessmentRecord)
+    application_id = picked_row.application_id
+    sub_criteria_id = "app-info"
+
+    assessment_payload = {
+        "application_id": application_id,
+        "sub_criteria_id": sub_criteria_id,
+        "score": 5,
+        "justification": "great",
+        "user_id": "test"
+    }
+    score_metadata = create_just_score_for_application_sub_crit(**assessment_payload)
+
+    latest_score_metadata = get_latest_just_score_for_application_sub_crit(application_id, sub_criteria_id)
+
+    assert latest_score_metadata.get("date_created") == score_metadata.get("date_created")
+    assert latest_score_metadata.get("score") == score_metadata.get("score")
+    assert latest_score_metadata.get("justification") == score_metadata.get("justification")
