82d7defef7ac9dab8f75e275dea2acc0cb7fbc4a
Indi Harrington <indigoharrington@gmail.com>
2022-12-05 09:27:08 +0000

    🐛 Fixed bug

--- scripts/import_from_application.py
+++ scripts/import_from_application.py
@@ -1,19 +1,30 @@
 #!/usr/bin/env python3
-from app import app
-import requests
-from config import Config
 import argparse
+
+import requests
+from app import app
+from config import Config
 from db.queries import bulk_insert_application_record
 
-parser = argparse.ArgumentParser(description='Import applcations from application store.')
+parser = argparse.ArgumentParser(
+    description="Import applcations from application store."
+)
 parser.add_argument("--roundid", type=str)
 
 args = parser.parse_args()
 
 with app.app_context():
 
-    applications_url = Config.APPLICATIONS_ENDPOINT + "?status_only=SUBMITTED" + f"?round_id={args.roundid}"
+    applications_url = (
+        Config.APPLICATIONS_ENDPOINT
+        + "?status_only=SUBMITTED"
+        + f"?round_id={args.roundid}"
+    )
 
-    form_jsons_list = requests.get(applications_url).json["forms"]
+    app_store_response_json = requests.get(applications_url).json
 
-    bulk_insert_application_record(form_jsons_list)
+    for application in app_store_response_json:
+
+        form_jsons_list = app_store_response_json["forms"]
+
+        bulk_insert_application_record(form_jsons_list)
