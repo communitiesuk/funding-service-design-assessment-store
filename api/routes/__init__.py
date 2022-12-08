71781bf83dcd1394f0afca7a9fe0ab8f849cf618
Ram <80714392+ramsharma-prog@users.noreply.github.com>
2022-12-08 12:48:21 +0000

    imported function to init file

--- api/routes/__init__.py
+++ api/routes/__init__.py
@@ -1,8 +1,11 @@
 from .assessment_routes import all_assessments_for_fund_round_id
 from .assessment_routes import get_assessor_task_list_state
 from .assessment_routes import sub_criteria
+
 from .score_routes import latest_score_for_application_sub_criteria
 from .score_routes import post_score_for_application_sub_criteria
+
+from .assessment_routes import get_sub_criteria_theme_answers
 
 __all__ = [
     "all_assessments_for_fund_round_id", 
@@ -10,4 +13,5 @@ __all__ = [
     "post_score_for_application_sub_criteria",
     "get_assessor_task_list_state",
     "sub_criteria",
+    "get_sub_criteria_theme_answers",
     ]
