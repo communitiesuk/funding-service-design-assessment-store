fd6eeb658e3db7b2f35761e8ef88e7b0c0adabe3
Sarah Sloan <sarah.sloan@levellingup.gov.uk>
2022-12-22 17:02:13 +0000

    added skeleton unit test

---
+++ tests/test_cof_import_mapper.py
@@ -0,0 +1,13 @@
+import json
+
+from db.queries.assessment_records._helpers import derive_values_from_json
+
+
+def test_derive_cof_values():
+
+    single_app = "tests/test_data/single_application.json"
+
+    with open(single_app, "r") as f:
+        loaded_json = json.load(f)
+    derived_fields = derive_values_from_json(loaded_json, "COF")
+    assert "TEST-REF" == derived_fields["short_id"], "Wrong short id"
