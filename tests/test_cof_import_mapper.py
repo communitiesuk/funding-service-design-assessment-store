486d128e73132479ce7568e3959ea9745d096f2d
John O'Neill <john.oneill@version1.com>
2023-01-03 12:54:53 +0000

    FS-2111: Updated test data for cof import mapper unit test

--- tests/test_cof_import_mapper.py
+++ tests/test_cof_import_mapper.py
@@ -4,10 +4,10 @@ from db.queries.assessment_records._helpers import derive_values_from_json
 
 
 def test_derive_cof_values():
-
     single_app = "tests/test_data/single_application.json"
 
     with open(single_app, "r") as f:
         loaded_json = json.load(f)
     derived_fields = derive_values_from_json(loaded_json, "COF")
+    # should this assertion not be the other way around?
     assert "TEST-REF" == derived_fields["short_id"], "Wrong short id"
