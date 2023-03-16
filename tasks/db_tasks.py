2ba5dc518ec05a6ee05f86335522c472af3e76b3
tferns <thomas.ferns@version1.com>
2023-03-16 12:43:13 +0000

    BAU: Pre-commit maintenance

--- tasks/db_tasks.py
+++ tasks/db_tasks.py
@@ -91,20 +91,24 @@ def seed_dev_db(c, fundround=None, appcount=None):
             if not choosing:
                 fund_round = config[fundround]
                 apps = int(appcount)
-                print(f"Seeding {apps} applications for fund_round: '{fundround}'")
+                print(f"Seeding {apps} applications "
+                      f"for fund_round: '{fundround}'")
 
             while choosing:
 
                 new_line = '\n'
                 _echo_print(
-                    f"fund-rounds available to seed: {new_line} - {f' {new_line} - '.join(config.keys())}",
+                    f"fund-rounds available to seed: "
+                    f"{new_line} - {f' {new_line} - '.join(config.keys())}",
                 )
-                fund_round_input = str(_echo_input("Please type the fund-round to seed:"))
+                fund_round_input = str(_echo_input("Please type the "
+                                                   "fund-round to seed:"))
                 fund_round = config[fund_round_input]
                 apps = int(_echo_input("How many applications?"))
                 choosing = (
                     not _echo_input(
-                        f"Would you like to insert {apps} applications for {fund_round_input}? y/n \n"
+                        f"Would you like to insert {apps} "
+                        f"applications for {fund_round_input}? y/n \n"
                     ).lower()
                     == "y"
                 )
