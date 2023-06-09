70ea02e0a6afe9af93911238e1fd193a53a3da0a
RamuniN <navya.ramuni@communities.gov.uk>
2023-06-09 11:59:38 +0100

    make section to flag to array type

--- db/models/flags/flags.py
+++ db/models/flags/flags.py
@@ -6,6 +6,7 @@ import uuid
 from db import db
 from db.models.flags.enums import FlagType
 from sqlalchemy import ForeignKey
+from sqlalchemy.dialects.postgresql import ARRAY
 from sqlalchemy.dialects.postgresql import ENUM
 from sqlalchemy.dialects.postgresql import UUID
 from sqlalchemy.sql import func
@@ -35,4 +36,7 @@ class Flag(db.Model):
 
     justification = db.Column("justification", db.Text(), nullable=True)
 
-    section_to_flag = db.Column("section_to_flag", db.Text(), nullable=True)
+    # section_to_flag = db.Column("section_to_flag", ARRAY(db.String(256)), nullable=True)
+    sections_to_flag = db.Column(
+        "sections_to_flag", ARRAY(db.String(256)), nullable=True
+    )
