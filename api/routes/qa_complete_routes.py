7ac377c3b5eaeaa25efa22030f657c1977b14a30
Gio Karunakaran <gio.karunakaran@version1.com>
2023-07-13 15:44:49 +0100

    fs-3056-refactor-QA-complete

--- api/routes/qa_complete_routes.py
+++ api/routes/qa_complete_routes.py
@@ -1,4 +1,5 @@
 # flake8: noqa
+from db.queries.assessment_records.queries import get_metadata_for_application
 from db.queries.qa_complete.queries import create_qa_complete_record
 from db.queries.qa_complete.queries import (
     get_qa_complete_record_for_application,
@@ -8,12 +9,19 @@ from db.queries.qa_complete.queries import (
 def post_qa_complete_for_application(
     application_id: str, user_id: str
 ) -> dict:
-    created_qa_complete_record = create_qa_complete_record(
-        application_id=application_id,
-        user_id=user_id,
-    )
-
-    return created_qa_complete_record
+    application_json = get_metadata_for_application(application_id)
+    application_workflow_status = application_json.get("workflow_status") 
+    if application_workflow_status == "COMPLETED" and 404 in get_qa_complete_record_for_application(application_id):
+        created_qa_complete_record = create_qa_complete_record(
+            application_id=application_id,
+            user_id=user_id,
+        )
+        return created_qa_complete_record 
+    return {
+        "code": 401,
+        "message": f"Could not create qa_complete record for application with id: {application_id}",
+        "status": "error",
+        }, 401 
 
 
 def qa_complete_record_for_application(application_id: str) -> dict:
