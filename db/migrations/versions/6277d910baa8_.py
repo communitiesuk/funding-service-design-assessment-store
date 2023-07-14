c058c9034e22f766de0b5a2fa7e1522f058a6f0c
Gio Karunakaran <gio.karunakaran@version1.com>
2023-07-14 09:44:37 +0100

    fs-3056-refactor-QA-complete

--- db/migrations/versions/6277d910baa8_.py
+++ db/migrations/versions/6277d910baa8_.py
@@ -7,7 +7,6 @@ Create Date: 2023-07-12 11:09:23.553438
 """
 import sqlalchemy as sa
 from alembic import op
-from alembic_utils.pg_extension import PGExtension
 from sqlalchemy.dialects import postgresql
 
 # revision identifiers, used by Alembic.
