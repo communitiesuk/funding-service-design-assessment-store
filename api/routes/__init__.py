fb34eaca5afdde1202c2b53a6a9fd8529b0f9413
Adam Davies <adam.davies@version1.com>
2023-07-27 15:39:49 +0100

    Running pre commit

--- api/routes/__init__.py
+++ api/routes/__init__.py
@@ -4,9 +4,9 @@ from .assessment_routes import assessment_metadata_for_application_id
 from .assessment_routes import assessment_stats_flagsv2_for_fund_round_id
 from .assessment_routes import assessment_stats_for_fund_round_id
 from .assessment_routes import create_flag_v2_for_application
-from .assessment_routes import get_application_data_for_export
 from .assessment_routes import get_all_flags_v2_for_application
 from .assessment_routes import get_all_uploaded_document_theme_answers
+from .assessment_routes import get_application_data_for_export
 from .assessment_routes import get_application_json
 from .assessment_routes import get_assessor_task_list_state
 from .assessment_routes import get_banner_state
