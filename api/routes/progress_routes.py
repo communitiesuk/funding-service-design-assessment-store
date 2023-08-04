a44320994ea66a137e517b8b44f5c7490d72d810
Adam <adam.wallace@communities.gov.uk>
2023-08-04 13:46:11 +0100

    add fund_round specification to application progress stats

--- api/routes/progress_routes.py
+++ api/routes/progress_routes.py
@@ -1,19 +1,22 @@
+import copy
 from typing import List
 
-from config.mappings.cof_mapping_parts.cof_r2_scored_criteria import (
-    scored_criteria,
-)
+from config import Config
 from db.queries.progress.queries import get_progress_for_app
 from flask import request
 
 
-def post_progress_for_applications() -> List[dict]:
+def post_progress_for_applications(fund_id, round_id) -> List[dict]:
     application_ids = request.get_json()["application_ids"]
-    return get_progress_for_applications(application_ids)
+    return get_progress_for_applications(application_ids, fund_id, round_id)
 
 
-def get_progress_for_applications(application_ids: List[str]) -> List[dict]:
-
+def get_progress_for_applications(
+    application_ids: List[str], fund_id, round_id
+) -> List[dict]:
+    scored_criteria = copy.deepcopy(
+        Config.ASSESSMENT_MAPPING_CONFIG[f"{fund_id}:{round_id}"]
+    )["scored_criteria"]
     scored_sub_crit_list = [
         subcrit["id"]
         for crit in scored_criteria
