cb5e2ce3bfd49ed0f8a0347afdf529bc06adf732
RamuniN <navya.ramuni@communities.gov.uk>
2023-08-23 13:16:15 +0100

    updated tests for sqs queue

--- app.py
+++ app.py
@@ -47,6 +47,22 @@ def create_app() -> Flask:
     health.add_check(FlaskRunningChecker())
     health.add_check(DbChecker(db))
 
+    # # Configurations for Flask-Apscheduler
+    # from flask_apscheduler import APScheduler
+    # scheduler = APScheduler()
+
+    # flask_app.config["SCHEDULER_API_ENABLED"] = True
+    # flask_app.config["JOBS"] = [
+    #     {
+    #         "id": "sqs_process_queues",
+    #         "func": "_helpers:import_applications_from_queue",
+    #         "trigger": "interval",
+    #         "seconds": 60,  # Run the job every 60 seconds
+    #     }
+    # ]
+    # scheduler.init_app(flask_app)
+    # scheduler.start()
+
     return flask_app
 
 
--- tests/test_sqs_helpers.py
+++ tests/test_sqs_helpers.py
@@ -1,81 +1,110 @@
-# import unittest
-# from unittest.mock import Mock
-# from unittest.mock import patch
+import json
+
 import pytest
 from _helpers import delete_messages
+from _helpers import import_applications_from_queue
 from _helpers import receive_messages
-
-# from botocore.exceptions import ClientError
+from config.mappings.assessment_mapping_fund_round import (
+    fund_round_mapping_config,
+)
+from tests._helpers import row_data
 
 
 @pytest.fixture(scope="function")
-def mock_sqs_recieve_message(mocker):
+def mock_sqs_recieve_message(request, mocker):
+    fundround = request.node.get_closest_marker("fundround").args[0]
+    appcount = request.node.get_closest_marker("appcount").args[0]
+
+    fund_round_config = {fundround: fund_round_mapping_config[fundround]}
+    application_json_strings = row_data(appcount, 1, 1, fund_round_config)
+    messages = []
+    for ind, application_json in enumerate(application_json_strings):
+        messages.append(
+            {
+                "MessageId": str(ind + 1),
+                "Body": json.loads(application_json),
+                "ReceiptHandle": str(ind),
+            }
+        )
 
     mocker.patch(
         "_helpers._SQS_CLIENT.receive_message",
+        return_value={"Messages": messages},
+    )
+    yield messages
+
+
+@pytest.fixture(scope="function")
+def mock_sqs_delete_message(request, mocker):
+    appcount = request.node.get_closest_marker("appcount").args[0]
+    mocker.patch(
+        "_helpers._SQS_CLIENT.delete_message_batch",
         return_value={
-            "Messages": [
-                {"MessageId": "1", "Body": "Message 1"},
-                {"MessageId": "2", "Body": "Message 2"},
-            ]
+            "Successful": [{"Id": str(count)} for count in range(appcount)]
         },
     )
     yield
 
 
 @pytest.fixture(scope="function")
-def mock_sqs_delete_message(mocker):
-
-    mocker.patch(
-        "_helpers._SQS_CLIENT.delete_message_batch",
-        return_value={"Successful": [{"Id": "0"}, {"Id": "1"}], "Failed": []},
-    )
-    yield
+def mock_bulk_insert_application_records(mocker, mock_sqs_recieve_message):
+    messages = mock_sqs_recieve_message
+    mock_db_session = [
+        {
+            "application_id": msg["Body"]["id"],
+            "round_id": msg["Body"]["round_id"],
+            "short_ref": msg["Body"]["reference"],
+        }
+        for msg in messages
+    ]
+    with (
+        mocker.patch(
+            "_helpers.import_application.bulk_insert_application_record",
+            return_value=mock_db_session,
+        )
+    ):
+        yield
 
 
 class TestSQSFunctions(object):
-    def test_receive_messages(self, mock_sqs_recieve_message):
+    @pytest.mark.fundround("NSTFR2")
+    @pytest.mark.appcount(3)
+    def test_receive_messages(self, request, mock_sqs_recieve_message):
+        appcount = request.node.get_closest_marker("appcount").args[0]
+
         # Call the function
         messages = receive_messages(max_number=2)
 
         # Assertions
-        assert len(messages) == 2
+        assert len(messages) == appcount
         assert messages[0]["MessageId"] == "1"
-        assert messages[1]["Body"] == "Message 2"
 
-    # @patch("_helpers._SQS_CLIENT")
-    # def test_receive_messages_no_messages(self, mock_sqs_client):
-    #     # Mock response from SQS with no messages
-    #     mock_response = {"ResponseMetadata": {"HTTPStatusCode": 200}}
-    #     mock_sqs_client.receive_message.return_value = mock_response
+    @pytest.mark.appcount(3)
+    def test_delete_messages(self, request, mock_sqs_delete_message):
+        appcount = request.node.get_closest_marker("appcount").args[0]
 
-    #     # Call the function
-    #     messages = receive_messages(max_number=2)
-
-    #     # Assertions
-    #     assert messages is None
-
-    # @patch("_helpers._SQS_CLIENT")
-    # def test_receive_messages_error(self, mock_sqs_client):
-    #     # Mock ClientError
-    #     mock_sqs_client.receive_message.side_effect = ClientError({}, "ReceiveMessage")
-
-    #     # Call the function and assert it raises an error
-    #     with self.assertRaises(ClientError):
-    #         receive_messages(max_number=2)
-
-    def test_delete_messages(self, mock_sqs_delete_message):
         # Call the function
-        response = delete_messages(["receipt_handle_0", "receipt_handle_1"])
+        receipt_handles = [
+            "receipt_handle_" + str(count) for count in range(appcount)
+        ]
+        response = delete_messages(receipt_handles)
 
         # Assertions
-        assert len(response["Successful"]) == 2
+        assert len(response["Successful"]) == appcount
 
-    # @patch("_helpers._SQS_CLIENT")
-    # def test_delete_messages_error(self, mock_sqs_client):
-    #     # Mock ClientError
-    #     mock_sqs_client.delete_message_batch.side_effect = ClientError({}, "DeleteMessageBatch")
+    @pytest.mark.fundround("NSTFR2")
+    @pytest.mark.appcount(3)
+    def test_import_application_queue(
+        self,
+        request,
+        mock_sqs_recieve_message,
+        mock_sqs_delete_message,
+        mock_bulk_insert_application_records,
+    ):
+        appcount = request.node.get_closest_marker("appcount").args[0]
 
-    #     # Call the function and assert it doesn't raise an error
-    #     response = delete_messages(["receipt_handle_0", "receipt_handle_1"])
-    #     self.assertIsNone(response)
+        # Call the function
+        response = import_applications_from_queue()
+
+        # Assertions
+        assert len(response) == appcount
