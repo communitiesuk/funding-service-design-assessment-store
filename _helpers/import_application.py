a21fcdac59cb757c490ed6908119bb140f3110dc
RamuniN <navya.ramuni@communities.gov.uk>
2023-08-31 14:22:30 +0100

    exit early & remove indentation

--- _helpers/import_application.py
+++ _helpers/import_application.py
@@ -18,52 +18,51 @@ def import_applications_from_queue():
         _SQS_QUEUE_URL, batch_size, visibility_time, wait_time
     )
 
-    application_json_list = []
+    if not application_messages:
+        return
 
-    if application_messages:
-        with app.app.app_context():
-            for message in application_messages:
-                application_json = message["Body"]
-                if isinstance(application_json, str):
-                    application_json = json.loads(application_json)
-                application_json_list.append(application_json)
+    with app.app.app_context():
+        application_json_list = []
+        for message in application_messages:
+            application_json = message["Body"]
+            if isinstance(application_json, str):
+                application_json = json.loads(application_json)
+            application_json_list.append(application_json)
 
-            # bulk insert the applications
-            inserted_applications = bulk_insert_application_record(
-                application_json_list,
-                is_json=True,
+        # bulk insert the applications
+        inserted_applications = bulk_insert_application_record(
+            application_json_list,
+            is_json=True,
+        )
+        insert_application_ids = [
+            application["application_id"]
+            for application in inserted_applications
+        ]
+
+        # delete the messages from queue
+        reciept_handles_to_delete = []
+        for message in application_messages:
+            if (
+                message["MessageAttributes"]["application_id"]["StringValue"]
+                in insert_application_ids
+            ):
+                reciept_handles_to_delete.append(message["ReceiptHandle"])
+        if reciept_handles_to_delete:
+            delete_messages(_SQS_QUEUE_URL, reciept_handles_to_delete)
+
+        for message in application_messages:
+            receive_count = int(
+                message["Attributes"]["ApproximateReceiveCount"]
             )
-            insert_application_ids = [
-                application["application_id"]
-                for application in inserted_applications
-            ]
-
-            # delete the messages from queue
-            reciept_handles_to_delete = []
-            for message in application_messages:
-                if (
-                    message["MessageAttributes"]["application_id"][
-                        "StringValue"
-                    ]
-                    in insert_application_ids
-                ):
-                    reciept_handles_to_delete.append(message["ReceiptHandle"])
-            if reciept_handles_to_delete:
-                delete_messages(_SQS_QUEUE_URL, reciept_handles_to_delete)
-
-            for message in application_messages:
-                receive_count = int(
-                    message["Attributes"]["ApproximateReceiveCount"]
+            if receive_count == Config.AWS_DLQ_MAX_RECIEVE_COUNT:
+                current_app.logger.error(
+                    f"Failed to import application with id: "
+                    f"{message['MessageAttributes']['application_id']['StringValue']}. "
+                    "Moving message to DLQ"
                 )
-                if receive_count == Config.AWS_DLQ_MAX_RECIEVE_COUNT:
-                    current_app.logger.error(
-                        f"Failed to import application with id: "
-                        f"{message['MessageAttributes']['application_id']['StringValue']}. "
-                        "Moving message to DLQ"
-                    )
 
-            # return inserted applications
-            return inserted_applications
+        # return inserted applications
+        return inserted_applications
 
 
 if __name__ == "__main__":
