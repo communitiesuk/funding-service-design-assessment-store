714e6a747a403931bfe0772a63f83908f8c13727
Adam Davies <adam.davies@version1.com>
2024-01-04 16:10:16 +0000

    Updating script

--- scripts/insert_scoring_system.py
+++ scripts/insert_scoring_system.py
@@ -5,6 +5,9 @@ import argparse
 from app import app  # noqa: E402
 from db.queries.scores.queries import insert_scoring_system_for_round_id
 
+# Update scoring systems when new ones are added
+scoring_systems = ["OneToFive"]
+
 
 def init_argparse() -> argparse.ArgumentParser:
     parser = argparse.ArgumentParser(
@@ -16,7 +19,26 @@ def init_argparse() -> argparse.ArgumentParser:
         help="Provide round id of the fund.",
         required=False,
     )
+
+    parser.add_argument(
+        "--scoring_system",
+        type=str,
+        choices=scoring_systems,
+        help="Provide round id of the fund.",
+        required=False,
+    )
     return parser
+
+
+def display_scoring_systems():
+    BLUE = "\033[94m"
+    RESET = "\033[0m"
+    print(f"- {BLUE}Available scoring systems:{RESET}")
+    for system in scoring_systems:
+        print(f"- {BLUE}{system}{RESET}")
+
+    choice = input("Choose a scoring system from the list: ")
+    return choice.strip() if choice in scoring_systems else None
 
 
 def main() -> None:
@@ -24,15 +46,23 @@ def main() -> None:
     args = parser.parse_args()
 
     if args.round_id:
-        roundid = args.round_id
+        round_id = args.round_id
     else:
         raise Exception("Please provide all the required arguments.")
 
     with app.app_context():
-        inserted_scoring_system = insert_scoring_system_for_round_id(
-            roundid, "OneToFive"
-        )
-        return inserted_scoring_system
+        selected_scoring_system = display_scoring_systems()
+        if selected_scoring_system:
+            inserted_scoring_system = insert_scoring_system_for_round_id(
+                round_id, selected_scoring_system
+            )
+            print(inserted_scoring_system)
+            print("Scoring system inserted successfully")
+            return inserted_scoring_system
+        else:
+            raise ValueError(
+                "Invalid choice for scoring system. Please choose from the list."
+            )
 
 
 if __name__ == "__main__":
