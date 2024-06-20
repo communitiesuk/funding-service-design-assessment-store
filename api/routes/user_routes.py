2decb62c1c04c93af1833cc8c1e73aa4253c50c2
Kishan Patel <kishanpatel@Kishans-Air.broadband>
2024-06-20 14:22:13 +0100

    Add API routes for user application association

---
+++ api/routes/user_routes.py
@@ -0,0 +1,14 @@
+def get_all_users_associated_with_application():
+    pass
+
+
+def get_user_application_association():
+    pass
+
+
+def add_user_application_association():
+    pass
+
+
+def update_user_application_association():
+    pass
